c3665bbfa76a64640091805f6b81ec46e41a56b6
tong <bluedragon00000@gmail.com>
2021-09-09 20:12:53 +0800

    0909-3

--- miniprogram/pages/common/home/home.js
+++ miniprogram/pages/common/home/home.js
@@ -13,20 +13,33 @@ const app = getApp();
 Page({
 
   data: {
-    // 数据
+    // 公共数据
     gw: 0,
     lastGw: 0,
     nextGw: 0,
     entry: 0,
-    entryName: '',
+    entryInfoData: {},
+    titleGw: 0,
+    title: '',
+    tab: 'index',
+    // index页
     time: 0,
     deadline: "",
     timeData: {},
     fixtureList: [],
-    dreamTeamList: [],
-    eliteElmentList: [],
-    // tab
-    tab: '',
+    // dreamTeam页
+    dreamTeamGkpList: [],
+    dreamTeamDefList: [],
+    dreamTeamMidList: [],
+    dreamTeamFwdList: [],
+    // elite页
+    eliteElmentGkpList: [],
+    eliteElmentDefList: [],
+    eliteElmentMidList: [],
+    eliteElmentFwdList: [],
+    // transfers页
+    transfersInList: [],
+    transfersOutList: [],
     // refresh
     pullDownRefresh: false,
   },
@@ -43,17 +56,12 @@ Page({
         lastGw: app.globalData.lastGw,
         nextGw: app.globalData.nextGw,
         entry: app.globalData.entry,
-        entryName: app.globalData.entryInfoData.entryName,
+        entryInfoData: app.globalData.entryInfoData,
         deadline: app.globalData.deadline,
         time: diffDeadlineTime(app.globalData.utcDeadline)
       });
       if (this.data.entry <= 0) {
         redirectToEntryInput();
-      }
-      // 设置标题
-      let entryName = this.data.entryName;
-      if (entryName === '' || typeof entryName === 'undefined') {
-        this.initEntryInfo();
       }
       // 拉取赛程
       this.initNextGwFixture();
@@ -61,6 +69,10 @@ Page({
       this.initDreamTeam();
       // 拉取高分球员
       this.initEliteElements();
+      // 拉取转会
+      this.initEventTransfers();
+      // 设置标题
+      this.setNavTitle();
     });
   },
 
@@ -81,27 +93,69 @@ Page({
         entryName: app.globalData.entryInfoData.entryName,
         deadline: app.globalData.deadline,
         time: diffDeadlineTime(app.globalData.utcDeadline),
-        fixtureList: [],
-        dreamTeamList: [],
-        eliteElmentList: [],
       });
-      let tab = this.data.tab;
-      if (tab === 'fixture') {
-        // 拉取赛程
-        this.initNextGwFixture();
-      } else if (tab === 'dreamTeam') {
-        // 拉取梦之队
-        this.initDreamTeam();
-      } else if (tab === 'elite') {
-        // 拉取高分球员
-        this.initEliteElements();
-      }
+      // 拉取赛程
+      this.initNextGwFixture();
+      // 拉取梦之队
+      this.initDreamTeam();
+      // 拉取高分球员
+      this.initEliteElements();
+      // 拉取转会
+      this.initEventTransfers();
     });
   },
 
   /**
    * 操作
    */
+
+  // 设置导航栏标题
+  setNavTitle() {
+    let tab = this.data.tab;
+    switch (tab) {
+      case 'index': {
+        let entryName = this.data.entryInfoData.entryName;
+        if (entryName === '' || typeof entryName === 'undefined') {
+          this.initEntryInfo();
+        }
+        this.setData({
+          titleGw: this.data.gw,
+          title: entryName
+        });
+        break;
+      }
+      case 'dreamTeam': {
+        this.setData({
+          titleGw: this.data.lastGw,
+          title: "GW" + this.data.lastGw + " - 梦之队"
+        });
+        break;
+      }
+      case 'elite': {
+        this.setData({
+          titleGw: this.data.lastGw,
+          title: "GW" + this.data.lastGw + " - 高分球员"
+        });
+        break;
+      }
+      case 'transfers': {
+        this.setData({
+          titleGw: this.data.lastGw,
+          title: "GW" + this.data.lastGw + " - 转会榜"
+        });
+        break;
+      }
+    }
+  },
+
+  // 标签页切换
+  tabOnChange(event) {
+    let tab = event.detail.name;
+    this.setData({
+      tab: tab
+    });
+    this.setNavTitle();
+  },
 
   // 页面切换
   onSwipperChange(event) {
@@ -115,10 +169,13 @@ Page({
     if (tab === 'fixture' && this.data.fixtureList.length === 0) {
       // 拉取赛程
       this.initNextGwFixture();
-    } else if (tab === 'dreamTeam' && this.data.dreamTeamList.length === 0) {
+    } else if (tab === 'dreamTeam' && this.data.dreamTeamGkpList.length === 0) {
       // 拉取梦之队
       this.initDreamTeam();
-    } else if (tab === 'elite' && this.data.eliteElmentList.length === 0) {
+    } else if (tab === 'elite' && this.data.eliteElmentGkpList.length === 0) {
+      // 拉取高分球员
+      this.initEliteElements();
+    } else if (tab === 'transfers' && this.data.transfersInList.length === 0) {
       // 拉取高分球员
       this.initEliteElements();
     }
@@ -186,8 +243,11 @@ Page({
         entry: this.data.entry
       }, false)
       .then(res => {
+        let data = res.data;
         this.setData({
-          entryName: res.data.entryName
+          entryInfoData: data,
+          titleGw: this.data.gw,
+          title: data.entryName
         });
       })
       .catch(res => {
@@ -200,9 +260,37 @@ Page({
         event: this.data.lastGw
       })
       .then(res => {
-        let list = res.data;
+        let list = res.data,
+          gkpList = [],
+          defList = [],
+          midList = [],
+          fwdList = [];
+        list.forEach(element => {
+          let elementType = element.elementType;
+          switch (elementType) {
+            case 1: {
+              gkpList.push(element);
+              break;
+            }
+            case 2: {
+              defList.push(element);
+              break;
+            }
+            case 3: {
+              midList.push(element);
+              break;
+            }
+            case 4: {
+              fwdList.push(element);
+              break;
+            }
+          }
+        });
         this.setData({
-          dreamTeamList: list
+          dreamTeamGkpList: gkpList,
+          dreamTeamDefList: defList,
+          dreamTeamMidList: midList,
+          dreamTeamFwdList: fwdList
         })
       })
       .catch(res => {
@@ -215,14 +303,61 @@ Page({
         event: this.data.lastGw
       })
       .then(res => {
-        let list = res.data;
+        let list = res.data,
+          gkpList = [],
+          defList = [],
+          midList = [],
+          fwdList = [];
+        list.forEach(element => {
+          let elementType = element.elementType;
+          switch (elementType) {
+            case 1: {
+              gkpList.push(element);
+              break;
+            }
+            case 2: {
+              defList.push(element);
+              break;
+            }
+            case 3: {
+              midList.push(element);
+              break;
+            }
+            case 4: {
+              fwdList.push(element);
+              break;
+            }
+          }
+        });
         this.setData({
-          eliteElmentList: list
+          eliteElmentGkpList: gkpList,
+          eliteElmentDefList: defList,
+          eliteElmentMidList: midList,
+          eliteElmentFwdList: fwdList,
         });
       })
       .catch(res => {
         console.log('fail:', res);
       })
+  },
+
+  initEventTransfers() {
+    get('common/qryEventOverallTransfers', {
+      event: this.data.lastGw
+    })
+    .then(res => {
+      let list = res.data,
+        inList = [],
+        outList = []
+      list.forEach(element => {
+ 
+      this.setData({
+     
+      })
+    })
+    .catch(res => {
+      console.log('fail:', res);
+    })
   },
 
 });
