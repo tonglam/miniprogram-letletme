4f7ba11ec1dd3df00762d36ed0c55b380da4f309
tong <bluedragon00000@gmail.com>
2021-09-09 17:13:55 +0800

    0909-2

--- miniprogram/pages/common/home/home.js
+++ miniprogram/pages/common/home/home.js
@@ -15,13 +15,18 @@ Page({
   data: {
     // 数据
     gw: 0,
+    lastGw: 0,
+    nextGw: 0,
     entry: 0,
     entryName: '',
     time: 0,
-    nextGw: 0,
     deadline: "",
     timeData: {},
     fixtureList: [],
+    dreamTeamList: [],
+    eliteElmentList: [],
+    // tab
+    tab: '',
     // refresh
     pullDownRefresh: false,
   },
@@ -35,22 +40,27 @@ Page({
     delay(400).then(() => {
       this.setData({
         gw: app.globalData.gw,
+        lastGw: app.globalData.lastGw,
+        nextGw: app.globalData.nextGw,
         entry: app.globalData.entry,
         entryName: app.globalData.entryInfoData.entryName,
-        nextGw: app.globalData.nextGw,
         deadline: app.globalData.deadline,
         time: diffDeadlineTime(app.globalData.utcDeadline)
       });
       if (this.data.entry <= 0) {
         redirectToEntryInput();
       }
-      // 拉取赛程
-      this.getNextGwFixture();
       // 设置标题
       let entryName = this.data.entryName;
       if (entryName === '' || typeof entryName === 'undefined') {
         this.initEntryInfo();
       }
+      // 拉取赛程
+      this.initNextGwFixture();
+      // 拉取梦之队
+      this.initDreamTeam();
+      // 拉取高分球员
+      this.initEliteElements();
     });
   },
 
@@ -65,20 +75,54 @@ Page({
     delay(400).then(() => {
       this.setData({
         gw: app.globalData.gw,
+        lastGw: app.globalData.lastGw,
+        nextGw: app.globalData.nextGw,
         entry: app.globalData.entry,
         entryName: app.globalData.entryInfoData.entryName,
-        nextGw: app.globalData.nextGw,
         deadline: app.globalData.deadline,
-        time: diffDeadlineTime(app.globalData.utcDeadline)
+        time: diffDeadlineTime(app.globalData.utcDeadline),
+        fixtureList: [],
+        dreamTeamList: [],
+        eliteElmentList: [],
       });
-      // 拉取赛程
-      this.getNextGwFixture();
+      let tab = this.data.tab;
+      if (tab === 'fixture') {
+        // 拉取赛程
+        this.initNextGwFixture();
+      } else if (tab === 'dreamTeam') {
+        // 拉取梦之队
+        this.initDreamTeam();
+      } else if (tab === 'elite') {
+        // 拉取高分球员
+        this.initEliteElements();
+      }
     });
   },
 
   /**
    * 操作
    */
+
+  // 页面切换
+  onSwipperChange(event) {
+    let tab = event.detail.currentItemId;
+    if (tab === '') {
+      return false;
+    }
+    this.setData({
+      tab: tab
+    });
+    if (tab === 'fixture' && this.data.fixtureList.length === 0) {
+      // 拉取赛程
+      this.initNextGwFixture();
+    } else if (tab === 'dreamTeam' && this.data.dreamTeamList.length === 0) {
+      // 拉取梦之队
+      this.initDreamTeam();
+    } else if (tab === 'elite' && this.data.eliteElmentList.length === 0) {
+      // 拉取高分球员
+      this.initEliteElements();
+    }
+  },
 
   // 倒计时变化
   onChange(event) {
@@ -102,7 +146,28 @@ Page({
    * 数据
    */
 
-  getNextGwFixture() {
+  refreshEventAndDeadline() {
+    get('common/refreshEventAndDeadline')
+      .then(() => {
+        // 下拉刷新
+        if (this.data.pullDownRefresh) {
+          wx.stopPullDownRefresh({
+            success: () => {
+              Toast({
+                type: 'success',
+                duration: 1000,
+                message: "刷新成功"
+              });
+              this.setData({
+                pullDownRefresh: false
+              });
+            },
+          });
+        }
+      });
+  },
+
+  initNextGwFixture() {
     get('common/qryNextFixture', {
         event: this.data.gw
       })
@@ -130,25 +195,34 @@ Page({
       });
   },
 
-  refreshEventAndDeadline() {
-    get('common/refreshEventAndDeadline')
-      .then(() => {
-        // 下拉刷新
-        if (this.data.pullDownRefresh) {
-          wx.stopPullDownRefresh({
-            success: () => {
-              Toast({
-                type: 'success',
-                duration: 1000,
-                message: "刷新成功"
-              });
-              this.setData({
-                pullDownRefresh: false
-              });
-            },
-          });
-        }
-      });
-  }
+  initDreamTeam() {
+    get('common/qryEventDreamTeam', {
+        event: this.data.lastGw
+      })
+      .then(res => {
+        let list = res.data;
+        this.setData({
+          dreamTeamList: list
+        })
+      })
+      .catch(res => {
+        console.log('fail:', res);
+      })
+  },
+
+  initEliteElements() {
+    get('common/qryEventEliteElements', {
+        event: this.data.lastGw
+      })
+      .then(res => {
+        let list = res.data;
+        this.setData({
+          eliteElmentList: list
+        });
+      })
+      .catch(res => {
+        console.log('fail:', res);
+      })
+  },
 
 });
